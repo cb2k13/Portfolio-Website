17d2a0357ffaa486e04de53adc5c0ed58746ab2e
Carlos Briceno <carlosbriceno@Carloss-MacBook-Pro.local>
2025-09-01 13:47:58 -0700

    Fixed Nav

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,52 +1,62 @@
-
 import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
 import LogoC from '../../assets/images/logo-C.png'
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser, faComputer } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
+const Sidebar = () => {
+  const [open, setOpen] = useState(false)
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
+  return (
+    <div className={`nav-bar ${open ? 'open' : ''}`}>
+      <Link className='logo' to='/' onClick={() => setOpen(false)}>
         <img src={LogoC} alt="logo" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
+      </Link>
 
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </NavLink>
+      {/* Hamburger is visible only on mobile via CSS */}
+      <button
+        className="hamburger"
+        type="button"
+        aria-label="Toggle menu"
+        aria-expanded={open}
+        onClick={() => setOpen(o => !o)}
+      >☰</button>
 
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </NavLink>
+      <nav onClick={() => setOpen(false)}>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="skills-link" to="/skills">
-            <FontAwesomeIcon icon={faComputer} color="4d4d4e" />
-            </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
 
-        </nav>
-    <ul>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink exact="true" activeclassname="active" className="skills-link" to="/skills">
+          <FontAwesomeIcon icon={faComputer} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+
+      <ul>
         <li>
-            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/carlos-briceno-a87a71167/" >
-                <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
-            </a>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/carlos-briceno-a87a71167/">
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
         </li>
-
         <li>
-            <a target="_blank" rel="noreferrer" href="https://github.com/cb2k13" >
-                <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
-            </a>
+          <a target="_blank" rel="noreferrer" href="https://github.com/cb2k13">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
         </li>
-
-
-    </ul>
-
+      </ul>
     </div>
-)
+  )
+}
 
-export default Sidebar 
+export default Sidebar
+
