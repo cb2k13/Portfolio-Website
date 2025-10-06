99e145d44c226a2a48fd4954d4866404ddcf5636
Carlos Briceno <carlosbriceno33.2@gmail.com>
2025-10-06 15:07:29 -0700

    Great

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import About from './components/About'
 import Contact from './components/Contact'
 import Skills from './components/Skills'
+import Projects from './components/Projects'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
     <Route path="about" element={<About />} />
     <Route path="contact" element={<Contact />} />
     <Route path="skills" element={<Skills />} />
+    <Route path="projects" element={<Projects />} />
     </Route>
    </Routes>
    
